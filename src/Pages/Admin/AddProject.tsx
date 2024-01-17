eddbd0e0a6e736fc6eb94e03ec6d8008ca37f0bb
Meesam <meesam.engineer@gmail.com>
2024-01-17 18:52:07 +0530

    wroking on Add project page

--- src/Pages/Admin/AddProject.tsx
+++ src/Pages/Admin/AddProject.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { ToastContainer, toast } from "react-toastify";
-import { Button } from "@radix-ui/themes";
-import { TbWorldUp } from "react-icons/tb";
 import AddProjectBasicInfo from "./components/AddProjectBasicInfo";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
 
 const AddProject = () => {
   return (
     <form className="flex flex-col gap-4">
-      <ToastContainer />
-      <div className="flex justify-between items-center">
-        <h1
-          className="font-bold text-xl text-gray-600"
-          data-cy="create-new-project"
-        >
+      <Box className="flex justify-between items-center">
+        <Typography variant="h5" data-cy="create-new-project">
           Create New Project
-        </h1>
-      </div>
-      <div className="flex gap-4">
-        {/* Basic info */}
-        <AddProjectBasicInfo />
-      </div>
+        </Typography>
+      </Box>
+      <Box>
+        <Grid container spacing={2}>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <AddProjectBasicInfo />
+          </Grid>
+        </Grid>
+      </Box>
     </form>
   );
 };
--- src/Pages/Admin/components/AddProjectBasicInfo.tsx
+++ src/Pages/Admin/components/AddProjectBasicInfo.tsx
@@ -1,20 +1,33 @@
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import {
+  Button,
+  TextField,
+  Box,
+  Paper,
+  Typography,
+  Autocomplete,
+} from "@mui/material";
 import React from "react";
-import { CustomerBasicInfo } from "../../../Interface";
+import { CustomerBasicInfo, Project } from "../../../Interface";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FaSave } from "react-icons/fa";
 import { useCustomer } from "../../../Context/CustomerContext";
 import { customerBasicInfoSchema } from "../../../ValidationSchema";
+import { useQuery } from "@tanstack/react-query";
+import { getAllCustomers } from "../../../Services/CustomerService";
 
 const AddProjectBasicInfo = () => {
   const { addCustomerBasicInfo } = useCustomer();
+  const { data: customers } = useQuery({
+    queryKey: ["customers"],
+    queryFn: getAllCustomers,
+  });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<CustomerBasicInfo>({
+  } = useForm<Partial<Project>>({
     resolver: zodResolver(customerBasicInfoSchema),
   });
 
@@ -23,104 +36,95 @@ const AddProjectBasicInfo = () => {
   };
 
   return (
-    <div className="flex flex-col gap-4 bg-white h-auto p-5 rounded-md shadow-md w-1/2">
-      <h1 className=" font-semibold text-lg text-gray-600">Basic Info</h1>
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="name"
-          className="text-sm font-medium"
-          data-cy="customer-name-label"
-        >
-          Customer Name
-        </label>
-        <TextField.Input
-          type="text"
-          id="name"
-          placeholder="Enter customer name"
-          {...register("name")}
-          data-cy="customer-name-input"
-        />
-        {errors.name && (
-          <span className="text-xs text-red-500" data-cy="name-error">
-            {errors.name.message}
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="website"
-          className="text-sm font-medium"
-          data-cy="website-label"
-        >
-          Website
-        </label>
-        <TextField.Input
-          type="text"
-          id="website"
-          placeholder="Enter website name"
-          {...register("website")}
-          data-cy="website-input"
-        />
-        {errors.website && (
-          <span className="text-xs text-red-500" data-cy="website-error">
-            {errors.website.message}
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="logoUrl"
-          className="text-sm font-medium"
-          data-cy="LogoUrl-label"
-        >
-          LogoUrl
-        </label>
-        <TextField.Input
-          type="text"
-          id="logoUrl"
-          placeholder="Enter logo url"
-          {...register("logoUrl")}
-          data-cy="LogoUrl-input"
-        />
-        {errors.logoUrl && (
-          <span className="text-xs text-red-500" data-cy="logoUrl-error">
-            {errors.logoUrl.message}
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col gap-1">
-        <label
-          htmlFor="description"
-          className="text-sm font-medium"
-          data-cy="Description-label"
-        >
-          Description
-        </label>
-        <TextArea
-          size="3"
-          id="description"
-          placeholder="Description"
-          {...register("description")}
-          data-cy="Description-input"
-        />
-        {errors.description && (
-          <span className="text-xs text-red-500" data-cy="description-error">
-            {errors.description.message}
-          </span>
-        )}
-      </div>
-      <div className="flex flex-col gap-1 items-end">
-        <div className="w-auto">
-          <Button
-            type="button"
-            onClick={handleSubmit(addBasicDetail)}
-            data-cy="addContact-button"
-          >
-            <FaSave /> Save
-          </Button>
-        </div>
-      </div>
-    </div>
+    <Paper>
+      <Box className="flex flex-col gap-4 h-auto p-5">
+        <Typography variant="h6">Basic Info</Typography>
+        <Box className="flex flex-col gap-1">
+          <Autocomplete
+            id="customer-select-"
+            options={customers || []}
+            autoHighlight
+            getOptionLabel={(option) => option.name}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select Customer"
+                inputProps={{
+                  ...params.inputProps,
+                }}
+                {...register("customerName")}
+              />
+            )}
+          />
+          {errors.customerName && (
+            <span className="text-xs text-red-500" data-cy="name-error">
+              {errors.customerName.message}
+            </span>
+          )}
+        </Box>
+        <Box className="flex flex-col gap-1">
+          <TextField
+            type="text"
+            id="website"
+            placeholder="Enter website name"
+            {...register("website")}
+            data-cy="website-input"
+            label="Website"
+            variant="outlined"
+          />
+          {errors.website && (
+            <span className="text-xs text-red-500" data-cy="website-error">
+              {errors.website.message}
+            </span>
+          )}
+        </Box>
+        <Box className="flex flex-col gap-1">
+          <TextField
+            type="text"
+            id="logoUrl"
+            placeholder="Enter logo url"
+            {...register("logoUrl")}
+            data-cy="LogoUrl-input"
+            label="LogoUrl"
+            variant="outlined"
+          />
+          {errors.logoUrl && (
+            <span className="text-xs text-red-500" data-cy="logoUrl-error">
+              {errors.logoUrl.message}
+            </span>
+          )}
+        </Box>
+        <Box className="flex flex-col gap-1">
+          <TextField
+            id="description"
+            placeholder="Description"
+            {...register("description")}
+            data-cy="Description-input"
+            label="Description"
+            variant="outlined"
+          />
+          {errors.description && (
+            <span className="text-xs text-red-500" data-cy="description-error">
+              {errors.description.message}
+            </span>
+          )}
+        </Box>
+        <Box className="flex flex-col gap-1 items-end">
+          <Box className="w-auto">
+            <Button
+              type="button"
+              onClick={handleSubmit(addBasicDetail)}
+              data-cy="addContact-button"
+              startIcon={<FaSave />}
+              variant="contained"
+              color="secondary"
+            >
+              Save
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Paper>
   );
 };
 
